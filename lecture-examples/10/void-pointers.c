f539516e58f7c014a7a1b3480333c362baf8d512
Julian M. Kunkel <juliankunkel@googlemail.com>
2019-10-31 21:19:41 +0000

    Examples

---
+++ lecture-examples/10/void-pointers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum type_e{
+  TYPE_INTEGER
+};
+
+// Void pointer example
+void inc_generic(void * p, enum type_e typ){
+  switch(typ){
+    case(TYPE_INTEGER):{
+      int * i = (int*) p;
+      (*i)++;
+      break;
+    }default:{
+      printf("Unknown type\n");
+      exit(0);
+    }
+  }
+}
+
+int main(int argc, char ** argv){
+  int x = 5;
+  inc_generic(&x, TYPE_INTEGER);
+  printf("x=%d\n", x);
+  return 0;
+}
