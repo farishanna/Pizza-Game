540801cf714a93309e520f5fc8950874e9fca34d
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-03-12 16:36:57 +0000

    Nai

--- lecture-examples/spring/10/meta-gcd.cpp
+++ lecture-examples/spring/10/meta-gcd.cpp
@@ -11,12 +11,12 @@ template< int a > struct GCD< a, 0 > {
     static const int RESULT = a;
 };
 
-
+GCD<25, 50> var;
 
 int main() {
   cout << "GCD (25,50) == " << GCD<25, 50>::RESULT << endl;
   cout << "GCD (12,64) == " << GCD<12, 64>::RESULT << endl;
   cout << "GCD (a,b) == " << GCD<18, 8398176>::RESULT << endl;
-  
+
   return 0;
 }
