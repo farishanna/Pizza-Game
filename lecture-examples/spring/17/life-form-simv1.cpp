871baa1fca10993f9529019b41b5ee1a098f2b05
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-03-20 11:52:03 +0000

    Added timestep (which fixes the calculation).

--- lecture-examples/spring/17/life-form-simv1.cpp
+++ lecture-examples/spring/17/life-form-simv1.cpp
@@ -12,15 +12,15 @@ struct CycleOutput{
   CycleOutput(float prey, float predator) : prey(prey), predator(predator) {}
 };
 
-vector<CycleOutput> simulate(int cycles, float x=10, float y=10, float alpha = 1.1, float beta = 0.4, float delta = 0.1, float gamma = 0.4){
+vector<CycleOutput> simulate(int cycles, float timestep = 0.1, float x=10, float y=10, float alpha = 1.1, float beta = 0.4, float delta = 0.1, float gamma = 0.4){
   vector<CycleOutput> output;
-  // simulate 10 cycles
+  output.push_back(CycleOutput(x, y));
   for(int i=0; i < cycles; i++){
     // change in a timestep
-    float nx = alpha * x - beta * x * y;
-    float ny = delta * x * y - gamma * y;
-    x = nx;
-    y = ny;
+    float nx = timestep * (alpha * x - beta * x * y);
+    float ny = timestep * (delta * x * y - gamma * y);
+    x += nx;
+    y += ny;
     if(x < 0) x = 0;
     if(y < 0) y = 0;
 
@@ -36,7 +36,7 @@ void printSimOutput(vector<CycleOutput> const & result){
 }
 
 void testCase1(){
-  auto result = simulate(1, 10, 0, 1.2);
+  auto result = simulate(1, 1.0, 10, 0, 1.2);
   assert(result[0].prey == 12.0);
   assert(result[0].predator == 0);
 }
@@ -50,7 +50,7 @@ void testCase1(){
 int main(int argc, char ** argv){
   //testCase1();
 
-  auto result = simulate(10);
+  auto result = simulate(400);
   printSimOutput(result);
   return 0;
 }
