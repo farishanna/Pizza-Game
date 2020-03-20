340375f4ebaf98e33e8c67643daeda796dc61bc1
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-03-20 09:25:06 +0000

    Simulation

---
+++ lecture-examples/spring/17/life-forms-taxonomy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// this is a taxonomy of life forms
+// https://manoa.hawaii.edu/exploringourfluidearth/biological/what-alive/classification-life
+// Other characteristics of a life form:
+// https://study.com/academy/lesson/8-characteristics-of-life-in-biology.html
+
+class Lifeform{
+  
+};
+
+int main(){
+
+  return 0;
+}
